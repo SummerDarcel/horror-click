a90bcc35da9123a552366bfed7d8ff37dcad5b35
SummerMedusa <50718811+SummerDarcel@users.noreply.github.com>
2019-10-29 18:21:43 -0700

    Updates

---
+++ precache-manifest.e4fe24e39ac8906f7326667f66824e8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f0e6eefaa295535c72f06c9cde0956",
+    "url": "/horror-click/index.html"
+  },
+  {
+    "revision": "e21eb7b29cfa33376292",
+    "url": "/horror-click/static/css/main.2962bdcf.chunk.css"
+  },
+  {
+    "revision": "38e83522fb3191735994",
+    "url": "/horror-click/static/js/2.9d3b6063.chunk.js"
+  },
+  {
+    "revision": "e21eb7b29cfa33376292",
+    "url": "/horror-click/static/js/main.79df56a2.chunk.js"
+  },
+  {
+    "revision": "0b089b2df3ae078b9b63",
+    "url": "/horror-click/static/js/runtime-main.6209bd7b.js"
+  }
+]);
